0bb6bd59a46ab6d8305e3d2a4d7a064377fdffed
toweldaijin <colourfulflower21@gmail.com>
2022-05-31 10:30:32 +0900

    #1

--- src/Components/CountriesSearchPage.jsx
+++ src/Components/CountriesSearchPage.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import Grid from '@mui/material/Grid';
 
 
 
 export const CountriesSearchPage = () => {
     return(
         <>
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '0' }}
+        >
+            
             <p>国名で検索</p>
             <input type="search" id="search-box" placeholder="国名を入力"></input>
             <button>検索</button>
+            
+        </Grid>
+            
         </>
     );
 }

0f7e02c45041ee22949ac3728dba082abfec99b9
toweldaijin <colourfulflower21@gmail.com>
2022-07-02 04:52:40 +0900

    add

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,49 @@ import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 
+const years = [
+  {
+    value: 'before2012',
+    label: '2012年以前',
+  },
+  {
+    value: '2013',
+    label: '2013年',
+  },
+  {
+    value: '2014',
+    label: '2014年',
+  },
+  {
+    value: '2015',
+    label: '2015年',
+  },
+  {
+    value: '2016',
+    label: '2016年',
+  },
+  {
+    value: '2017',
+    label: '2017年',
+  },
+  {
+    value: '2018',
+    label: '2018年',
+  },
+  {
+    value: '2019',
+    label: '2019年',
+  },
+  {
+    value: '2020',
+    label: '2020年',
+  },
+  {
+    value: '2021',
+    label: '2021年',
+  },
+];
+
 const highschools = [
   {
     value: 'privateInCapitalArea',
@@ -42,24 +85,46 @@ const highschools = [
 ];
 
 
+
+
  const App = () => {
 
-  const [highschool, setHighschool] = useState('EUR');
+  const [year, setYear] = useState('before2012');
+  const handleChange1 = (event) => {
+    setYear(event.target.value);
+  };
 
-  const handleChange = (event) => {
+  const [highschool, setHighschool] = useState('privateInCapitalArea');
+  const handleChange4 = (event) => {
     setHighschool(event.target.value);
   };
 
   return(
     
    <>
+      <h2>受験年度</h2>
+       <TextField
+          id="outlined-select-years"
+          select
+          label="Select"
+          value={year}
+          onChange={handleChange1}
+          helperText="受験年度を選んでください"
+        >
+          {years.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+
       <h2>出身高校</h2>
        <TextField
           id="outlined-select-highschool"
           select
           label="Select"
           value={highschool}
-          onChange={handleChange}
+          onChange={handleChange4}
           helperText="出身高校の分類を選んでください"
         >
           {highschools.map((option) => (
@@ -68,6 +133,8 @@ const highschools = [
             </MenuItem>
           ))}
         </TextField>
+
+        
    </>
   );
 }
